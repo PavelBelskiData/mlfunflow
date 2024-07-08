83518fe3937ecb81d6b1d5b73f156ada45c7b575
Pavel Belski <belskiy.p@gmail.com>
2024-07-08 17:29:25 +0200

    1

--- modules/algorithms/__init__.py
+++ modules/algorithms/__init__.py
@@ -1,9 +1,2 @@
-#from .AlgorithmGenerator import AlgorithmGenerator
-#from .ElasticNetAlgorithmGenerator import ElasticNetAlgorithmGenerator
-
-import AlgorithmGenerator
-import ElasticNetAlgorithmGenerator
-
-a = ElasticNetAlgorithmGenerator()
-
-print('ok')
+from .AlgorithmGenerator import AlgorithmGenerator
+from .ElasticNetAlgorithmGenerator import ElasticNetAlgorithmGenerator
---
+++ py.py
@@ -0,0 +1,4 @@
+from modules.algorithms import AlgorithmGenerator
+
+
+print('ok')
